09c51f71cee01ad3a27346027f930587b2aa0d2a
Bob Lail <bob.lail@cph.org>
2013-12-14 14:47:23 -0600

    [improvement] Touched up styling of home page (20m)

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -2,7 +2,12 @@ class Stat < Struct.new(:skill, :level)
   
   delegate :name, :category, :description, to: :skill
   
+  def complete?
+    level.present?
+  end
+  
   def rank
+    return "none" unless complete?
     return "novice" if level < 24
     return "advanced-beginner" if level < 48
     return "competent" if level < 72
